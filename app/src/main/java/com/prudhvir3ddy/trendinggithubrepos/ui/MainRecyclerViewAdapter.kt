a7094cb5ca845d776cc284a281cd1a27dcae6a44
prudhvi reddy <prudhvireddy.m01@gmail.com>
2020-03-09 01:47:32 +0530

    working expanded item

--- app/src/main/java/com/prudhvir3ddy/trendinggithubrepos/ui/MainRecyclerViewAdapter.kt
+++ app/src/main/java/com/prudhvir3ddy/trendinggithubrepos/ui/MainRecyclerViewAdapter.kt
@@ -1,11 +1,12 @@
 package com.prudhvir3ddy.trendinggithubrepos.ui
 
 import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.GradientDrawable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
+import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
@@ -13,10 +14,8 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import com.prudhvir3ddy.trendinggithubrepos.R
 import com.prudhvir3ddy.trendinggithubrepos.database.UIResponse
+import com.prudhvir3ddy.trendinggithubrepos.databinding.ItemModelBinding
 import com.prudhvir3ddy.trendinggithubrepos.ui.MainRecyclerViewAdapter.MainRecyclerViewHolder
-import kotlinx.android.synthetic.main.item_model.view.description
-import kotlinx.android.synthetic.main.item_model.view.imageView
-import kotlinx.android.synthetic.main.item_model.view.name
 
 object DiffCallback : DiffUtil.ItemCallback<UIResponse>() {
   override fun areContentsTheSame(oldItem: UIResponse, newItem: UIResponse): Boolean {
@@ -29,34 +28,55 @@ object DiffCallback : DiffUtil.ItemCallback<UIResponse>() {
 }
 
 class MainRecyclerViewAdapter : ListAdapter<UIResponse, MainRecyclerViewHolder>(DiffCallback) {
-
+  private var mExpandedPosition = -1
+  private var mPreviousExpandedPosition = -1
   lateinit var context: Context
 
-  class MainRecyclerViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-    val imageView: ImageView = itemView.imageView
-    val name: TextView = itemView.name
-    val description: TextView = itemView.description
-  }
+  class MainRecyclerViewHolder(val binding: ItemModelBinding) :
+    RecyclerView.ViewHolder(binding.root)
 
   override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainRecyclerViewHolder {
     context = parent.context
-    return MainRecyclerViewHolder(
-      LayoutInflater.from(parent.context).inflate(
-        R.layout.item_model,
-        parent,
-        false
-      )
+    val binding: ItemModelBinding = DataBindingUtil.inflate(
+      LayoutInflater.from(context),
+      R.layout.item_model,
+      parent,
+      false
     )
+    return MainRecyclerViewHolder(binding)
   }
 
   override fun onBindViewHolder(holder: MainRecyclerViewHolder, position: Int) {
+    val isExpanded = position == mExpandedPosition
+    holder.binding.expandedItemContent.visibility = if (isExpanded) View.VISIBLE else View.GONE
+    holder.itemView.isActivated = isExpanded
+    if (isExpanded) {
+      mPreviousExpandedPosition = position
+    }
+    holder.binding.root.setOnClickListener {
+      mExpandedPosition = if (isExpanded) -1 else position
+      notifyItemChanged(mExpandedPosition)
+      notifyItemChanged(mPreviousExpandedPosition)
+    }
     val item = getItem(position)
+    val drawable: GradientDrawable =
+      holder.binding.languageIv.background.mutate() as GradientDrawable
+    val color = Color.parseColor(item.languageColor ?: "#000000")
+    drawable.setColor(color)
+
     Glide.with(context)
       .load(item.avatar)
       .apply(RequestOptions.circleCropTransform())
-      .into(holder.imageView)
-    holder.name.text = item.author
-    holder.description.text = item.name
+      .into(holder.binding.avatarIv)
+
+    holder.binding.apply {
+      authorTv.text = item.author
+      nameTv.text = item.name
+      descriptionTv.text = item.description
+      languageTv.text = item.language
+      starCountTv.text = item.stars.toString()
+      forkTv.text = item.forks.toString()
+    }
   }
 
 }
